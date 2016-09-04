f026a5fc80fdca1c79b197d35badeb12d5ccbfd7
Erik Nomitch <enomitch@gmail.com>
2016-09-04 00:24:30 -0500

    Moves every utility function to utility.h

--- src/Main.ino
+++ src/Main.ino
@@ -14,187 +14,31 @@
 // CONSTANTS -------------------------------------
 // -----------------------------------------------
 #define SERIAL_BAUD 115200
+#define SERVER_HTTP_PORT 80
 
 // -----------------------------------------------
 // GLOBALS ---------------------------------------
 // -----------------------------------------------
-
-// Lower-case version of hostname (this is set later)
-String hostnameLower = "";
-
-void ESP8266WebServer_redirect(ESP8266WebServer webServer, String path) {
-
-  // Send the new location in the header
-  webServer.sendHeader("Location", String("http://") + hostnameLower, true);
-
-  // Empty content inhibits Content-length header so we have to close the socket ourselves.
-  webServer.send ( 302, "text/plain", ""); 
-
-  // Stop is needed because we sent no content length
-  //webServer.client().stop(); 
-}
-
-/*ESP8266WebServer::redirect(String path) {*/
-
-/*void ESP8266WebServer::redirect(String path) {*/
-
-    /*this.sendHeader("Location", String("http://") + hostnameLower + path, true);*/
-    
-    /*// Empty content inhibits Content-length header so we have to close the socket ourselves.*/
-    /*this.send ( 302, "text/plain", ""); */
-
-    /*// Stop is needed because we sent no content length*/
-    /*this.client().stop(); */
-/*}*/
-
-ESP8266WebServer server(80);
-
-// -----------------------------------------------
-// GLOBALS->BLINK --------------------------------
-// -----------------------------------------------
-int ledState = LOW;
-long previousMillis = 0;
-long interval = 1000;
-
-// -----------------------------------------------
-// PUSHOVER --------------------------------------
-// -----------------------------------------------
-/*void sendPushNotification(String title, String message) {*/
-
-  /*HTTPClient http;*/
-  /*http.begin("http://api.pushover.net/1/messages.json");*/
-  /*http.addHeader("Content-Type", "application/x-www-form-urlencoded");*/
-
-  /*String postBody = "";*/
-
-  /*postBody = postBody + "token=" + PUSHOVER_TOKEN;*/
-  /*postBody = postBody + "&user="   + PUSHOVER_USER;*/
-  /*postBody = postBody + "&device=Laptop";*/
-  /*postBody = postBody + "&title="   + title;*/
-  /*postBody = postBody + "&message=" + message;*/
-  /*postBody = postBody + "&url=http://" + hostnameLower;*/
-  /*postBody = postBody + "&url_title=" + hostnameLower;*/
-  /*postBody = postBody + "&sound=none";*/
-
-  /*http.POST(postBody);*/
-  /*http.writeToStream(&Serial);*/
-  /*http.end();*/
-/*}*/
-
-// -----------------------------------------------
-// STATUS-LED ------------------------------------
-// -----------------------------------------------
-/*void setStatusLED() {*/
-  /*analogWrite(LED_STATUS, 50);*/
-/*}*/
-
-// -----------------------------------------------
-// SETUP->OTA ------------------------------------
-// -----------------------------------------------
-void setupOTA() {
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword((const char *)"123");
-
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-
-  ArduinoOTA.begin();
-}
-
-// -----------------------------------------------
-// SETUP->MDNS -----------------------------------
-// -----------------------------------------------
-void setupMDNS() {
-  // Set up mDNS responder:
-  // - first argument is the domain name, in this example
-  //   the fully-qualified domain name is "esp8266.local"
-  // - second argument is the IP address to advertise
-  //   we send our IP address on the WiFi network
-    /*
-  if (!MDNS.begin(hostnameLower)) {
-    Serial.println("Error setting up MDNS responder!");
-    while(1) { 
-      delay(1000);
-    }
-  } else {
-    Serial.println("mDNS responder started");
-  }
-    */
-}
+ESP8266WebServer server(SERVER_HTTP_PORT);
 
 // -----------------------------------------------
 // SETUP->WEB-SERVER -----------------------------
 // -----------------------------------------------
 void setupWebServer() {
+
+  // Path: /
+  // ---------------------------------------------
   server.on("/", [](){
-    String output = "Hello, Peef. Test: 3";
+    String output = "Hello, World.";
 
     server.send(200, "text/html", output);
   });
 
-  server.on("/off", [](){
-    // The builtin LED is REVERSED... HIGH is on.
-    digitalWrite(BUILTIN_LED, HIGH);
-    server.send(200, "text/html", "ok");
-  });
-  
-  server.on("/on", [](){
-    // The builtin LED is REVERSED... HIGH is on.
-    digitalWrite(BUILTIN_LED, LOW);
-    server.send(200, "text/html", "ok");
-  });
-
+  // Begin
+  // ---------------------------------------------
   server.begin();
 
   //MDNS.addService("http", "tcp", 80);
-}
-
-// -----------------------------------------------
-// SETUP->SERIAL ---------------------------------
-// -----------------------------------------------
-void setupSerial() {
-  Serial.begin(SERIAL_BAUD);
-}
-
-// -----------------------------------------------
-// SETUP->WI-FI ----------------------------------
-// -----------------------------------------------
-void setupWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_ESSID, WIFI_PASSWORD);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  hostnameLower = WiFi.hostname();
-  hostnameLower.toLowerCase();
 }
 
 // -----------------------------------------------
@@ -202,9 +46,9 @@ void setupWiFi() {
 // -----------------------------------------------
 void setup() {
 
-  digitalWrite(BUILTIN_LED, LOW);
+  setBuiltInLed(true);
 
-  setupSerial();
+  setupSerial(SERIAL_BAUD);
 
   setupWiFi();
 
--- src/utility.h
+++ src/utility.h
@@ -64,3 +64,131 @@ void clearSerial() {
   Serial.print("[2J");
 }
 
+// -----------------------------------------------
+// WEB-SERVER ------------------------------------
+// -----------------------------------------------
+//void ESP8266WebServer_redirect(ESP8266WebServer webServer, String path) {
+
+  //// Send the new location in the header
+  //webServer.sendHeader("Location", String("http://") + hostnameLower, true);
+
+  //// Empty content inhibits Content-length header so we have to close the socket ourselves.
+  //webServer.send ( 302, "text/plain", ""); 
+
+  //// Stop is needed because we sent no content length
+  ////webServer.client().stop(); 
+//}
+
+/*ESP8266WebServer::redirect(String path) {*/
+
+/*void ESP8266WebServer::redirect(String path) {*/
+
+    /*this.sendHeader("Location", String("http://") + hostnameLower + path, true);*/
+    
+    /*// Empty content inhibits Content-length header so we have to close the socket ourselves.*/
+    /*this.send ( 302, "text/plain", ""); */
+
+    /*// Stop is needed because we sent no content length*/
+    /*this.client().stop(); */
+/*}*/
+
+// -----------------------------------------------
+// MDNS ------------------------------------------
+// -----------------------------------------------
+void setupMDNS() {
+  // Set up mDNS responder:
+  // - first argument is the domain name, in this example
+  //   the fully-qualified domain name is "esp8266.local"
+  // - second argument is the IP address to advertise
+  //   we send our IP address on the WiFi network
+    /*
+  if (!MDNS.begin(hostnameLower)) {
+    Serial.println("Error setting up MDNS responder!");
+    while(1) { 
+      delay(1000);
+    }
+  } else {
+    Serial.println("mDNS responder started");
+  }
+    */
+}
+
+
+// -----------------------------------------------
+// OTA -------------------------------------------
+// -----------------------------------------------
+void setupOTA() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+}
+
+// -----------------------------------------------
+// SERIAL ----------------------------------------
+// -----------------------------------------------
+void setupSerial(int baudRate) {
+  Serial.begin(baudRate);
+}
+
+// -----------------------------------------------
+// SETUP->WI-FI ----------------------------------
+// -----------------------------------------------
+void setupWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_ESSID, WIFI_PASSWORD);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //hostnameLower = WiFi.hostname();
+  //hostnameLower.toLowerCase();
+}
+
+// ===============================================
+// ESP ===========================================
+// ===============================================
+
+// -----------------------------------------------
+// LED -------------------------------------------
+// -----------------------------------------------
+void setBuiltInLed(bool state) {
+  if (state) {
+    digitalWrite(BUILTIN_LED, LOW);
+    return;
+  }
+  digitalWrite(BUILTIN_LED, HIGH);
+}
